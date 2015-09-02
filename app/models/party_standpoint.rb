f58ea0f885089bc178cb37de3e47a566a64f4616
billy3321 <billy3321@gmail.com>
2015-09-03 00:02:27 +0800

    add validates

--- app/models/party_standpoint.rb
+++ app/models/party_standpoint.rb
@@ -3,10 +3,11 @@ class PartyStandpoint < ActiveRecord::Base
   belongs_to :issue
   validates_presence_of :party_id, message: '請選擇政黨'
   validates_presence_of :issue_id, message: '請選擇議題'
-  validates_presence_of :agree, message: '請填寫同意比例'
-  validates_presence_of :disagree, message: '請填寫反對比例'
-  validates_presence_of :abstain, message: '請填寫棄權比例'
-  validates_presence_of :notvote, message: '請填寫未投票比例'
+  validates_presence_of :agree, message: '請填寫同意數目'
+  validates_presence_of :disagree, message: '請填寫反對數目'
+  validates_presence_of :abstain, message: '請填寫棄權數目'
+  validates_presence_of :notvote, message: '請填寫未投票數目'
+  validates_presence_of :unknown, message: '請填寫不確定數目'
   validates_presence_of :decision, message: '請確定政黨立場'
   validates :decision, inclusion: { in: %w(agree disagree abstain notvote unknown),
     message: "立場字串不正確" }
